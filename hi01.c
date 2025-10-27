c7263865bce87a730e1afcad1a4349699399cd60
데스크탑 <@google.com>
2025-10-27 16:37:59 +0900

    hanbun mody

---
+++ hi01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)  {
+
+    int A, B;
+    double C;
+    printf("sfdf");
+
+    scanf("%d %d", &A, &B);
+
+    C = (double)A/(double)B;
+
+    printf("%lf", C);
+
+    return 0;
+}
